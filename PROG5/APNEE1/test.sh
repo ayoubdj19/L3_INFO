363177673568549af7135724296da8c526ac8a99
collomsu <39761559+collomsu@users.noreply.github.com>
2021-09-29 14:45:45 +0200

    update 29 09

---
+++ PROG5/APNEE1/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+resultat_attendu="Bon parenthesage"
+
+#max=ls programs/ | wc -l
+cpOK=0
+cpXX=0
+for file in programs/*
+do
+  ./$file <test.txt >resultat
+  if diff resultat resultat_attendu
+  then
+    ((cpOK+=1))
+    # echo OK
+  else
+    ((cpXX+=1))
+    # echo XXXX
+  fi
+done
+printf "\nProgrammes correct = $cpOK\nProgrammes incorrect = $cpXX\n"
