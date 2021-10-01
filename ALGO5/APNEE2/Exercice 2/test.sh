6c7c5afa31ad7c634b66bf8c558c84e668c5116c
collomsu <39761559+collomsu@users.noreply.github.com>
2021-10-01 15:57:19 +0200

    update

--- ALGO5/APNEE2/Exercice 2/test.sh
+++ ALGO5/APNEE2/Exercice 2/test.sh
@@ -4,40 +4,77 @@ cpOK=0
 cpXX=0
 isOK=0
 BON_P="Bon parenthesage"
-BON="Bon"
 MAUVAIS_P="Mauvais parenthesage"
-MAUVAIS="Mauvais"
 process="Running"
-
+i=0
 
 declare -A tab
 for file in programs/*
 do
   isOK=0
-  printf "$process\r"
-  for testNK in fichiers_test_generes/parenthesage_incorrect/*
+  ((i+=1))
+  printf "$i / 500\r"
+
+  for testNK in fichiers_test/parenthesage_correct/*
   do
     ./$file <$testNK >resultat
     resfile=$(cat resultat)
-    if [[ $resfile !=  $BON_P && $resfile !=  $BON ]]
+    if [[ $resfile !=  $BON_P ]]
     then
       isOK=1
       ((tab[$testNK]+=1))
+      break
     fi
   done
 
   if [ $isOK == 0 ]
   then
-    for testNK in fichiers_test_generes/parenthesage_incorrect/*
+
+    for testNK in fichiers_test/parenthesage_incorrect/*
     do
       ./$file <$testNK >resultat
       resfile=$(cat resultat)
-      if [[ $resfile !=  $MAUVAIS_P && $resfile !=  $MAUVAIS ]]
+      if [[ $resfile !=  $MAUVAIS_P ]]
       then
         isOK=1
         ((tab[$testNK]+=1))
+        break
       fi
     done
+
+    if [ $isOK == 0 ]
+    then
+
+      for testNK in fichiers_test_generes/parenthesage_correct/*
+      do
+        ./$file <$testNK >resultat
+        resfile=$(cat resultat)
+        if [[ $resfile !=  $BON_P ]]
+        then
+          isOK=1
+          ((tab[$testNK]+=1))
+          break
+        fi
+      done
+
+
+
+      if [ $isOK == 0 ]
+      then
+
+        for testNK in fichiers_test_generes/parenthesage_incorrect/*
+        do
+          ./$file <$testNK >resultat
+          resfile=$(cat resultat)
+          if [[ $resfile !=  $MAUVAIS_P ]]
+          then
+            isOK=1
+            ((tab[$testNK]+=1))
+            break
+          fi
+        done
+      fi
+    fi
   fi
 
   if [ $isOK == 0 ]
@@ -49,10 +86,10 @@ do
 
 done
 printf "\nNombre de programme qui ne réussise pas le test :\n"
-# for i in "${!tab[@]}"
-# do
-#   # j=${i#*fichiers_test/parenthesage_incorrect/}
-#   # j=${j#*fichiers_test/parenthesage_correct/}
-#   printf "\t$j = ${tab[$i]}\n"
-# done
+for i in "${!tab[@]}"
+do
+  j=${i#*fichiers_test/parenthesage_incorrect/}
+  j=${j#*fichiers_test/parenthesage_correct/}
+  printf "\t$j = ${tab[$i]}\n"
+done
 printf "\nProgrammes correct = $cpOK\nProgrammes incorrect = $cpXX\n"
