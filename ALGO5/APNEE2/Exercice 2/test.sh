88d315ac92ba800cfe332626e60732e6aee03667
collomsu <39761559+collomsu@users.noreply.github.com>
2021-10-01 15:24:46 +0200

    UPDATE

--- ALGO5/APNEE2/Exercice 2/test.sh
+++ ALGO5/APNEE2/Exercice 2/test.sh
@@ -7,31 +7,32 @@ BON_P="Bon parenthesage"
 BON="Bon"
 MAUVAIS_P="Mauvais parenthesage"
 MAUVAIS="Mauvais"
+process="Running"
+
 
 declare -A tab
 for file in programs/*
 do
   isOK=0
-
-  # for testOK in ../fichiers_test_generes/parenthesage_correct/*
-  # do
-  #   ./$file <$testOK >resultat
-  #   resfile=$(cat resultat)
-  #   if ["$resfile" !=  "$BON_P" && "$resfile" ==  "$BON"]
-  #   then
-  #     isOK=1
-  #     ((tab[$testOK]+=1))
-  #   fi
-  # done
+  printf "$process\r"
+  for testNK in fichiers_test_generes/parenthesage_incorrect/*
+  do
+    ./$file <$testNK >resultat
+    resfile=$(cat resultat)
+    if [[ $resfile !=  $BON_P && $resfile !=  $BON ]]
+    then
+      isOK=1
+      ((tab[$testNK]+=1))
+    fi
+  done
 
   if [ $isOK == 0 ]
   then
-    for testNK in ~/Documents/GitHub/L3_INFO/ALGO5/APNEE2/fichiers_test_generes/parenthesage_incorrect_trop_parentheses_fermantes/*
+    for testNK in fichiers_test_generes/parenthesage_incorrect/*
     do
-
       ./$file <$testNK >resultat
       resfile=$(cat resultat)
-      if ["$resfile" !=  "$MAUVAIS_P" && "$resfile" !=  "$MAUVAIS"]
+      if [[ $resfile !=  $MAUVAIS_P && $resfile !=  $MAUVAIS ]]
       then
         isOK=1
         ((tab[$testNK]+=1))
@@ -48,10 +49,10 @@ do
 
 done
 printf "\nNombre de programme qui ne réussise pas le test :\n"
-for i in "${!tab[@]}"
-do
-  # j=${i#*fichiers_test/parenthesage_incorrect/}
-  # j=${j#*fichiers_test/parenthesage_correct/}
-  printf "\t$j = ${tab[$i]}\n"
-done
+# for i in "${!tab[@]}"
+# do
+#   # j=${i#*fichiers_test/parenthesage_incorrect/}
+#   # j=${j#*fichiers_test/parenthesage_correct/}
+#   printf "\t$j = ${tab[$i]}\n"
+# done
 printf "\nProgrammes correct = $cpOK\nProgrammes incorrect = $cpXX\n"
