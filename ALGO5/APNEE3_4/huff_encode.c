5d5bdab6e2e04924eb8f7c6049016bd6aaeb7758
JohnTitour <60345060+JohnTitour@users.noreply.github.com>
2021-11-23 17:43:43 +0100

    Yow la street
    Commentaires ajoutés
    Structure de la fonction ConstruireCodeRec poffinée (même si il faut changer le fait que le code n'est pas une chaine de caractères mais une suite de 0 et de 1).
    Des bisous

--- ALGO5/APNEE3_4/huff_encode.c
+++ ALGO5/APNEE3_4/huff_encode.c
@@ -1,20 +1,18 @@
+
 #include "arbrebin.h"
 #include "bfile.h"
 #include "fap.h"
 #include "huffman_code.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
     int tab[256];
 } TableOcc_t;
 
 struct code_char HuffmanCode[256];
-
-char **t = malloc(sizeof(char*)*256);
-for(int i = 0; i < 256; i++){
-  t[i] = malloc(sizeof(char)*256);
-}
+typedef struct HuffmanCode_t;
 
 //L'attribut tab de TableOcc est remis à zéro en début de fonction
 void ConstruireTableOcc(FILE *fichier, TableOcc_t *TableOcc) {
@@ -26,7 +24,7 @@ void ConstruireTableOcc(FILE *fichier, TableOcc_t *TableOcc) {
     while (i < 256)
     {
         TableOcc->tab[i] = 0;
-
+        
         i = i + 1;
     }
 
@@ -57,7 +55,7 @@ fap InitHuffman(TableOcc_t *TableOcc) {
 
     int i = 0;
 
-    //On n'insère dans la file que les caractères apparaissant au moins une fois dans le texte.
+    //On n'insère dans la file que les caractères apparaissant au moins une fois dans le texte. 
     while (i < 256)
     {
         if(TableOcc->tab[i] > 0)
@@ -87,7 +85,7 @@ Arbre ConstruireArbre(fap file) {
     {
         //On extrait les deux éléments les plus prioritaires de la file à priorité
         file = extraire(file, &arbre1File, &prioriteArbre1File);
-
+        
         //Si il n'y avait qu'un seul élément dans la file, la construction de l'arbre est terminée.
         //Sinon, on continue l'exécution de l'algorithme
         if(!est_fap_vide(file))
@@ -100,7 +98,7 @@ Arbre ConstruireArbre(fap file) {
             //->Cet élément ne signifie rien de particulier, les caractères encodés par l'algorithme de Huffman étant positionnés sur les feuilles
 
             //On place arbitrairement le premier arbre extrait de la file à gauche et le second à droite.
-            file = inserer(file, NouveauNoeud(arbre1File, '_', arbre2File), prioriteArbre1File + prioriteArbre2File);
+            file = inserer(file, NouveauNoeud(arbre1File, 0, arbre2File), prioriteArbre1File + prioriteArbre2File);
         }
         //Cas dans lequel l'arbre de Huffman est construit
         else
@@ -113,26 +111,54 @@ Arbre ConstruireArbre(fap file) {
 }
 
 
-
-
-void ConstruireCodeRec(Arbre huff, char* w) {
+//Fonction appelée par la fonction "ConstruireCode" ci-dessous.
+//Le paramètre "debutCodage" représente le début de codage du mot déduit des bracnhes de l'arbre empruntées.
+//->lorsque l'on passe par un fils gauche pour un appel récursif->la chaine de caractère passée en paramètre est "debutCodage" avec un 0 en plus à la fin.
+//->lorsque l'on passe par un fils droit pour un appel récursif->la chaine de caractère passée en paramètre est "debutCodage" avec un 1 en plus à la fin.
+void ConstruireCodeRec(Arbre huff, char* debutCodage) {
+  //Un noeud d'un arbre de Huffman peut soit n'avoir aucun fils, soit en avoir 2.
+  
+  //Si le noeud sur lequel on se situe n'est pas une feuille, le codage des caractères continue
   if(!EstVide(FilsGauche(huff)) && !EstVide(FilsDroit(huff))) {
-    *w++;
-    *w = 0;
-    ConstruireCodeRec(FilsGauche(huff),w);
-    *w = 1;
-    ConstruireCodeRec(FilsDroit(huff),w);
-  } else {
+  	int tailleDebutCodage = strlen(debutCodage);
+  	
+  	//Création d'une chaine de caractères temporaire pour les appels récursifs.
+  	//+1 car le strlen ne compte pas le \0
+  	//+1 car la chaine de caractères crée a un caractère de plus que debutCodage
+  	char *motAppelRecursif = malloc((tailleDebutCodage + 1 + 1) * sizeof(char));
+  	strcpy(motAppelRecursif, debutCodage);
+  	motAppelRecursif[tailleDebutCodage + 1] = '\0';
+  	
+  	
+  	//Appel sur le fils gauche
+  	motAppelRecursif[tailleDebutCodage] = '0';
+  	ConstruireCodeRec(FilsGauche(huff), motAppelRecursif);
+  	
+  	//Appel sur le fils droit
+  	motAppelRecursif[tailleDebutCodage] = '1';
+  	ConstruireCodeRec(FilsDroit(huff), motAppelRecursif);
+  	
+  	
+  	//Libération de la chaine de caractères temporaire
+  	free(motAppelRecursif);
+  }
+  
+  //Sinon, le noeud sur lequel on se situe est une feuille, "debutCodage" pointe vers une chaine de caractères représentant le codage du symbole de la feuille.
+  else {
     int i = 0;
-    while (w[i] != '\0') {
-        t[huff.etiq][i] = w[i];
-    }
+    
+    //On copie la chaine pointée par "debutCodage" dans le tableau de codage (qui est un tableau de chaines de caractères) à l'indice du caractère codé
+    //Si une chaine de caractères éxiste déjà à cet indice, elle est libérée et remplacée.
+    
   }
 }
 
-void ConstruireCode(Arbre huff) {
-    ConstruireCodeRec(huff);
-    //printf("Programme non realise (ConstruireCode)\n");
+//Cette fonction retourne un pointeur vers le code de Huffman déduit de l'arbre de Huffman passé en paramètre.
+//Cette fonction utilise la fonction "ConstruireCodeRec" ci-dessus.
+code_char ConstruireCode(Arbre arbreDeHuffman) {
+	HuffmanCode_t* retour = malloc(sizeof(HuffmanCode_t*));
+
+	return ConstruireCodeRec(arbreDeHuffman, "");
 }
 
 void Encoder(FILE *fic_in, FILE *fic_out, Arbre ArbreHuffman) {
